b16beec36b407c49adb75ff53acc36a6bd03db9c
Siraj Ahmed <166845007+sirajahmedx@users.noreply.github.com>
2025-09-07 14:44:15 +0500

    add user model with complete schema and indexes

---
+++ modules/user/model.js
@@ -0,0 +1,102 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const addressSchema = new Schema({
+  city: { type: String, maxLength: 50 },
+  flat: { type: String, maxLength: 100 },
+  full_address: { type: String, maxLength: 200 },
+  is_default: { type: Boolean, default: false },
+  location: {
+    type: {
+      type: String,
+      enum: ["Point"],
+      default: "Point",
+    },
+    coordinates: {
+      type: [Number],
+    },
+  },
+});
+
+const userSchema = new Schema(
+  {
+    first_name: { type: String, maxLength: 50 },
+    last_name: { type: String, maxLength: 50 },
+    email: {
+      type: String,
+      maxLength: 50,
+      set: function (v) {
+        return v === "" ? null : v;
+      },
+    },
+    age: { type: Number, index: true, default: 0, maxLength: 3 },
+    role: {
+      type: String,
+      default: "user",
+      enum: ["mechanic", "admin", "user", "supplier"],
+    },
+    gender: {
+      type: String,
+      enum: ["male", "female"],
+      index: true,
+    },
+    cnic: { type: String, maxLength: 15 },
+    profile_picture: { type: String },
+    cnic_back_image: { type: String },
+    cnic_front_image: { type: String },
+    phone: {
+      type: String,
+      maxLength: 20,
+      unique: true,
+      sparse: true,
+      set: function (v) {
+        return v === "" ? null : v;
+      },
+    },
+    addresses: [addressSchema],
+    verified: [String],
+    status: { type: String, default: "not-approved" },
+    account_status: { type: String, default: "active" },
+    password: { type: String, maxLength: 100 },
+    salt: { type: String },
+    token: { type: String },
+    otp: { type: String },
+    otp_expiry: { type: Date },
+    job_counts: { type: Number, default: 0 },
+    experience: { type: Number, default: 0 },
+    // ratings: [ratingSchema],
+    avg_rating: { type: Number, default: 0 },
+    online: { type: Boolean, default: false },
+    lastSeen: { type: Date, default: Date.now },
+    // deviceTokens: [{ type: String, default: [] }],
+  },
+  {
+    timestamps: { createdAt: "created_at", updatedAt: "updated_at" },
+  }
+);
+
+userSchema.index(
+  { email: 1 },
+  {
+    unique: true,
+    partialFilterExpression: { email: { $exists: true, $ne: null, $ne: "" } },
+  }
+);
+userSchema.index(
+  { phone: 1 },
+  {
+    unique: true,
+    sparse: true,
+    partialFilterExpression: { phone: { $exists: true, $ne: null, $ne: "" } },
+  }
+);
+
+userSchema.index({ first_name: 1 });
+userSchema.index({ role: 1 });
+userSchema.index({ verified: 1 });
+userSchema.index({ status: 1 });
+userSchema.index({ skills: 1 });
+userSchema.index({ "addresses.location": "2dsphere" });
+
+module.exports =
+  mongoose.models.UserModel || mongoose.model("UserModel", userSchema);
